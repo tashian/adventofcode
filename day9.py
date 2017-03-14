d42709e6167374db6e657a3ae1294690bdb4355a
tashian <carl@tashian.com>
2017-03-14 09:26:47 -0700

    Day 9 now outputs TSP file for use with concorde TSP solver

--- day9.py
+++ day9.py
@@ -1,3 +1,9 @@
+# Depends on LKH TSP solver binary from http://www.akira.ruc.dk/~keld/research/LKH/
+# And on the pytsp package
+
+# Run this script as:
+#   LKH=path_to_LKH_binary python day9.py
+
 import re
 import random
 import itertools
@@ -20,36 +26,23 @@ def distance(i, j):
     if (all_nodes[i], all_nodes[j]) in edges:
         return edges[all_nodes[i], all_nodes[j]]
     else:
-        return float("inf")
+        return 0
 
-def solve_tsp_dynamic(points, distf):
-    # calc all lengths
-    n = len(points)
-    all_distances = [[distf(x,y) for y in xrange(n)] for x in xrange(n)]
-    # initial value - just distance from 0 to every other point + keep the track of edges
-    A = {(frozenset([0, idx+1]), idx+1): (dist, [0,idx+1]) for idx, dist in enumerate(all_distances[0][1:])}
-    cnt = len(points)
-    for m in range(2, cnt):
-        B = {}
-        for S in [frozenset(C) | {0} for C in itertools.combinations(range(1, cnt), m)]:
-            for j in S - {0}:
-                B[(S, j)] = min( [(A[(S-{j},k)][0] + all_distances[k][j], A[(S-{j},k)][1] + [j]) for k in S if k != 0 and k!=j])  #this will use 0th index of tuple for ordering, the same as if key=itemgetter(0) used
-                print m, S, j
-        A = B
-    res = min([(A[d][0] + all_distances[0][d[1]], A[d][1]) for d in iter(A)])
-    return res[1]
 
-path = solve_tsp_dynamic(all_nodes, distance)
-route = map(lambda x: all_nodes[x], path)
-print route
-prev_point = None
+n = len(all_nodes)
+matrix = [[distance(i, j) for i in xrange(n)] for j in xrange(n)]
 
-cost = 0
-for i, point in enumerate(route):
-    if i == 0:
-        prev_point = point
-        continue
-    cost += edges[prev_point, point]
-    prev_point = point
-print cost
+print all_nodes
+print matrix
+
+from pytsp import atsp_tsp, run, dumps_matrix
+
+matrix_sym = atsp_tsp(matrix, strategy="avg")
+
+outf = "/tmp/myroute.tsp"
+with open(outf, 'w') as dest:
+    dest.write(dumps_matrix(matrix_sym, name="My Route"))
+
+# tour = run(outf, start=10, solver="concorde")
+# print tour
 
