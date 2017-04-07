febf08b9d8e374f4f1d2d1316a581ec53c87d377
tashian <carl@tashian.com>
2017-04-07 06:18:57 -0700

    Day 1 part 2

--- day1.py
+++ day1.py
@@ -1,13 +1,14 @@
 def count_floors(directions):
     result = 0
-    for dir in list(directions):
+    for i, dir in enumerate(list(directions)):
         if dir == '(':
             result += 1
         elif dir == ')':
             result -= 1
+        if result == -1:
+            print "basement at position ", i+1
     return result
 
-print count_floors(directions)
 
 assert count_floors('()()') == 0
 assert count_floors('))(((((') == 3
@@ -16,4 +17,4 @@ assert count_floors(')())())') == -3
 i = open('day1.txt', 'r')
 directions = i.read()
 print len(directions)
-
+print count_floors(directions)
